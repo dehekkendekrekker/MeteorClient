8a387281711fa4f0dbc89513f622ae1f69b738e3
parallax <parallax@countermail.com>
2018-05-11 16:39:38 -0500

    - Added SSL

--- app/src/main/java/com/example/parallax/meteorclient/MainActivity.java
+++ app/src/main/java/com/example/parallax/meteorclient/MainActivity.java
@@ -5,6 +5,7 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.ListView;
 
 import com.example.parallax.meteorclient.factories.Task;
@@ -35,6 +36,10 @@ import javax.net.ssl.HostnameVerifier;
 import javax.net.ssl.SSLContext;
 import javax.net.ssl.TrustManagerFactory;
 
+import im.delight.android.ddp.Meteor;
+import im.delight.android.ddp.MeteorCallback;
+import im.delight.android.ddp.db.memory.InMemoryDatabase;
+
 //import im.delight.android.ddp.Meteor;
 //import im.delight.android.ddp.MeteorCallback;
 //import im.delight.android.ddp.db.memory.InMemoryDatabase;
@@ -51,12 +56,15 @@ public class MainActivity extends AppCompatActivity implements MeteorCallback {
 
     private ListView list;
 
+    private View rootView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        rootView = getWindow().getDecorView().getRootView();
 
-        test();
+        //test();
 
         list = (ListView) findViewById(R.id.list);
 
@@ -70,6 +78,17 @@ public class MainActivity extends AppCompatActivity implements MeteorCallback {
         // create a new instance
         mMeteor = new Meteor(this, "https://cuscopay.com/websocket", new InMemoryDatabase());
 
+        CertificateFactory cf;
+
+        try {
+            cf = CertificateFactory.getInstance("X.509");
+            InputStream caInput = new BufferedInputStream(getResources().openRawResource(R.raw.ca));
+            Certificate ca = cf.generateCertificate(caInput);
+            mMeteor.trustCaCert(ca);
+        } catch (CertificateException certException) {
+            System.out.print("DDP-SSL: " + certException.getMessage());
+        }
+
         // register the callback that will handle events and receive messages
         mMeteor.addCallback(this);
 
@@ -80,85 +99,13 @@ public class MainActivity extends AppCompatActivity implements MeteorCallback {
 
     }
 
-    private void test() {
 
-        WebSocketAdapter mWebSocketListener = new WebSocketAdapter() {
-
-            @Override
-            public void onConnected(final WebSocket websocket, final Map<String, List<String>> headers) {
-
-
-            }
-
-            @Override
-            public void onDisconnected(final WebSocket websocket, final WebSocketFrame serverCloseFrame, final WebSocketFrame clientCloseFrame, final boolean closedByServer) {
-
-            }
-
-
-            @Override
-            public void onError(final WebSocket websocket, final WebSocketException cause) {
-
-            }
-
-        };
-
-
-        try {
-            CertificateFactory cf = CertificateFactory.getInstance("X.509");
-
-            InputStream caInput = new BufferedInputStream(getResources().openRawResource(R.raw.ca));
-            Certificate ca;
-            try {
-                ca = cf.generateCertificate(caInput);
-                String test = "ca=" + ((X509Certificate) ca).getSubjectDN();
-
-                // Create a KeyStore containing our trusted CAs
-                String keyStoreType = KeyStore.getDefaultType();
-                KeyStore keyStore = KeyStore.getInstance(keyStoreType);
-                keyStore.load(null, null);
-                keyStore.setCertificateEntry("ca", ca);
-
-                // Create a TrustManager that trusts the CAs in our KeyStore
-                String tmfAlgorithm = TrustManagerFactory.getDefaultAlgorithm();
-                TrustManagerFactory tmf = TrustManagerFactory.getInstance(tmfAlgorithm);
-                tmf.init(keyStore);
-
-                // Create an SSLContext that uses our TrustManager
-                SSLContext context = SSLContext.getInstance("TLS");
-                context.init(null, tmf.getTrustManagers(), null);
-
-                WebSocket socket = new WebSocketFactory().setSSLSocketFactory(context.getSocketFactory()).createSocket("https://cuscopay.com/websocket");
-                socket.addListener(mWebSocketListener);
-                socket.connectAsynchronously();
-
-                WebSocketState state = socket.getState();
-
-
-            } catch (CertificateException exp) {
-                String msg = exp.getMessage();
-
-            } catch (Exception exp) {
-                String t = "efsf";
-            }
-
-            finally {
-                caInput.close();
-            }
-        } catch (Exception exception) {
-
-
-        }
-
-
-
-
-    }
 
 
     @Override
     public void onConnect(boolean signedInAutomatically) {
-        Snackbar snackbar = Snackbar.make(getWindow().getDecorView().getRootView(), "Connected ...", Snackbar.LENGTH_SHORT);
+
+        Snackbar snackbar = Snackbar.make(rootView, "Connected ...", Snackbar.LENGTH_SHORT);
         snackbar.show();
 
         String subscriptionId = mMeteor.subscribe("tasks");
@@ -171,7 +118,8 @@ public class MainActivity extends AppCompatActivity implements MeteorCallback {
 
     @Override
     public void onException(Exception e) {
-
+        Snackbar snackbar = Snackbar.make(rootView, "Could not connect to server, check connectivity", Snackbar.LENGTH_LONG);
+        snackbar.show();
     }
 
     @Override
